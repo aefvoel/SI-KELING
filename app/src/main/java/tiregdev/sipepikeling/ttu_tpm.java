eaeb674dc27d8eb697a62120b815f49c8ed3067f
JAR-FIS <fsandjujar@gmail.com>
2017-08-18 10:48:26 +0700

    push layout sikeling

--- app/src/main/java/tiregdev/sipepikeling/ttu_tpm.java
+++ app/src/main/java/tiregdev/sipepikeling/ttu_tpm.java
@@ -1,7 +1,10 @@
 package tiregdev.sipepikeling;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class ttu_tpm extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class ttu_tpm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ttu_tpm);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // todo: goto back activity from here
+                ttu_tpm.this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
